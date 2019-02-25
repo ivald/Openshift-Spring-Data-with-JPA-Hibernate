38dc4538216383c6179bc4a0d42ec5a4c45d672f
ilya.valdman <ivald79@gmail.com>
2019-02-25 15:06:57 -0500

    first commit

---
+++ src/test/java/com/tutorial/openshift/DatabaseTest.java
@@ -0,0 +1,60 @@
+package com.tutorial.openshift;
+
+import com.tutorial.openshift.service.Fruit;
+import com.tutorial.openshift.service.FruitRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("local")
+public class DatabaseTest {
+
+    @Autowired
+    private FruitRepository fruitRepository;
+
+    @Test
+    public void testGetAll() {
+        assertTrue(fruitRepository.findAll().spliterator().getExactSizeIfKnown() == 3);
+    }
+
+    @Test
+    public void getOne() {
+        assertNotNull(fruitRepository.findOne(1L));
+    }
+
+    @Test
+    public void updateAFruit() {
+        Fruit apple = fruitRepository.findOne(2L);
+        assertNotNull(apple);
+        assertEquals("Apple", apple.getName());
+
+        apple.setName("Green Apple");
+        fruitRepository.save(apple);
+
+        assertEquals("Green Apple", fruitRepository.findOne(2L).getName());
+    }
+
+    @Test
+    public void createAndDeleteAFruit() {
+        Long orangeId = fruitRepository.save(new Fruit("Orange")).getId();
+        Fruit orange = fruitRepository.findOne(orangeId);
+        assertNotNull(orange);
+
+        fruitRepository.delete(orange);
+        Fruit fruit = fruitRepository.findOne(orangeId);
+        assertNull(fruit);
+    }
+
+    @Test
+    public void getWrongId() {
+        Fruit fruit = fruitRepository.findOne(9999L);
+        assertNull(fruit);
+    }
+}
